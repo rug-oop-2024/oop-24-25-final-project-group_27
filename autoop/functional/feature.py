0f6b5a154a5b51abc84f607701ff6ea1bc9baa64
Thom0607 <thomboerboom@gmail.com>
2024-10-22 14:01:32 +0200

    Implemented method detect_feature_types in feature.py

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -3,6 +3,7 @@ from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
 
+
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
     Args:
@@ -10,8 +11,19 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     Returns:
         List[Feature]: List of features with their types.
     """
-    raise NotImplementedError("This should be implemented by you.")
+    features = []
     # Input is a Dataset
     # Use read function to get pandas data frame
+    pandas_frame = dataset.read()
     # Use dtypes function to get types of features
+    for column in pandas_frame.columns:
+        checked_feature = Feature(name=column)
+        if pandas_frame[column].dtype in ['int64', 'float64']:
+            # use setter of Feature class to set type to numerical
+            checked_feature.type("numerical")
+        else:
+            # use setter of Feature class to set type to categorical
+            checked_feature.type("categorical")
+        features.append(checked_feature)
     # Return List with features
+    return (features)

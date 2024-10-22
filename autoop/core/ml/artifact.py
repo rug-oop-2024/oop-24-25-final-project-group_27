0ee312173d15c5310f59a941eac91703cab9a109
Thom0607 <thomboerboom@gmail.com>
2024-10-22 16:22:42 +0200

    Started implementation of artifact.py

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -2,4 +2,14 @@ from pydantic import BaseModel, Field
 import base64
 
 class Artifact(BaseModel):
-    pass
+    #attributes to be implemented
+        # type
+        # name
+        # asset_path
+        # data in byte sequence
+        # version
+    # methods to be implemented
+        # read
+    def read(self) -> bytes:
+        # return the data in bytes of this artifact
+        return self.data

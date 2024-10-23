ba82e28d54d3535b0006f234a05716f304c724fa
Thom0607 <thomboerboom@gmail.com>
2024-10-23 14:59:19 +0200

    Started in metric.py

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, Field
 import base64
 
+
 class Artifact(BaseModel):
-    #attributes to be implemented
+    # attributes to be implemented
     type: str = Field(..., description="The type of the artfiact.")
     name: str = Field(None, description="Name of the artifact.")
     asset_path: str = Field(None, description="Path of where the artifact is stored.")
@@ -10,14 +11,12 @@ class Artifact(BaseModel):
     version: int = Field(None, description="Version of the artifact. Default is 1.0.0")
 
     # methods to be implemented
-    
+
     def read(self) -> bytes:
         # return the data in bytes of this artifact
         return self.data
 
     def save(self, data: bytes) -> bytes:
-        #save the data in bytes of this artifact and return the bytes of whole artifact.
+        # save the data in bytes of this artifact and return the bytes of whole artifact.
         self.data = data
-        
-    
-    
+        return self.data
--- autoop/core/ml/metric.py
+++ autoop/core/ml/metric.py
@@ -14,16 +14,28 @@ METRICS = [
 def get_metric(name: str):
     # Factory function to get a metric by name.
     # Return a metric instance given its str name.
-    raise NotImplementedError("To be implemented.")
+    if name == "mean_squared_error":
+        return MeanSquaredError()
+    elif name == "mean_absolute_error":
+        return MeanAbsoluteError()
+    elif name == "r_squared":
+        return RSquared()
+    elif name == "accuracy":
+        return Accuracy()
+    elif name == "precision":
+        return Precision()
+    elif name == "f1_score":
+        return F1Score()
 
 class Metric(ABC):
     """Base class for all metrics.
     """
     # your code here
     # remember: metrics take ground truth and prediction as input and return a real number
-    @abstractmethod
     def __call__(self):
-        raise NotImplementedError("To be implemented.")
+        pass
+
+    # wordt evaluate genoemd in pipeline.py dus aanpasssen en dan evaluate abstract en toepassen in concrete metrics
 
 # add here concrete implementations of the Metric class
 class MeanSquaredError(Metric):
@@ -38,7 +50,7 @@ class RSquared(Metric):
 class Accuracy(Metric):
 
 
-class Presicion(Metric):
+class Precision(Metric):
 
 class F1Score(Metric):
 

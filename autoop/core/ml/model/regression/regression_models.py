5844c40789973baf60242364028796f37b32b86e
Justus148 <152068026+Justus148@users.noreply.github.com>
2024-11-06 14:20:40 +0100

    implemnted SVR and linear.
    Co-authored-by: Thom0607 <Thom0607@users.noreply.github.com>

--- autoop/core/ml/model/regression/regression_models.py
+++ autoop/core/ml/model/regression/regression_models.py
@@ -3,11 +3,32 @@ import numpy as np
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-#first implementation of the regression model. 
-class SVR(Model):
+
+class SVRWrap(Model):
+
+    def __init__(self, parameters: dict) -> None:
+        """"Initialize SVR Wrapper Model"""
+
+        super().__init__(parameters)
+        self.model = SVR(**self._parameters)
+        
     def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
-        return super().fit(observations, ground_truth)
+        self.model.fit(observations, ground_truth)
 
     def predict(self, observations: np.ndarray) -> np.ndarray:
-        return super().predict(observations)
+        return self.model.predict(observations)
 
+class Linear_Regression(Model):
+        """Initialize Linear Regression Model"""
+
+    def __init__(self, parameters: dict) -> None:
+        super().__init__(parameters)
+        self.model = LinearRegression(**self._parameters)
+
+    def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
+        self.model.fit(observations, ground_truth)
+
+    def predict(self, observations: np.ndarray) -> np.ndarray:
+        return self.model.predict(observations)
+
+        

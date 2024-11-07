07c3bf34e34f245a7e54fcf9ad70fc58b954f490
Justus148 <152068026+Justus148@users.noreply.github.com>
2024-11-07 16:00:13 +0100

    final for 07-11

--- app/pages/1_📊_Datasets.py
+++ app/pages/1_📊_Datasets.py
@@ -13,9 +13,7 @@ st.write("Upload a CSV for processing.")
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-if uploaded_file is not None:
-    #Convert the given CSV into a Pandas DF to work with later
-    df = pd.read_csv(uploaded_file)
-    st.write(df)
-
+name = input("")
+new_dataset = Dataset()
+Dataset.from_dataframe(uploaded_file, name=name, )
 

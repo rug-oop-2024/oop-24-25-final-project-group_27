684fcef5df76c994897ef88fc20e47a386dd113f
Justus148 <152068026+Justus148@users.noreply.github.com>
2024-11-07 15:38:05 +0100

    got everything working without errors.
    Co-authored-by: Thom0607 <Thom0607@users.noreply.github.com>

---
+++ autoop/core/ml/model/classification/__init__.py
@@ -0,0 +1,3 @@
+from autoop.core.ml.model.classification.logistic_regression import LogisticRegressionWrapper
+from autoop.core.ml.model.classification.random_forest_classifier import RandomForestClassifierWrapper
+from autoop.core.ml.model.classification.svc import SVCWrapper
